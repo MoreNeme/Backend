9f8a08c5e734645a5a7e0668ecf61d5c3e7a2770
MoreNeme <marianonc@hotmail.com>
2023-02-02 17:26:23 -0300

    test

--- src/main/java/com/portfolio/mnc/controller/PersonaController.java
+++ src/main/java/com/portfolio/mnc/controller/PersonaController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,17 +38,17 @@ public class PersonaController {
         return new ResponseEntity(persona,HttpStatus.OK);
     }
     
-    /*@PostMapping("/create")
-    public ResponseEntity<?> create(@RequestBody dtoHys dtohys){
-        if(StringUtils.isBlank(dtohys.getNombre()))
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody dtoPersona dtopersona){
+        if(StringUtils.isBlank(dtopersona.getNombre()))
             return new ResponseEntity(new Mensaje("El nombre es obligatorio"),HttpStatus.BAD_REQUEST);
-        if(sHys.existsByNombre(dtohys.getNombre()))
+        if(personaService.existsByNombre(dtopersona.getNombre()))
             return new ResponseEntity(new Mensaje("esa skill existe"),HttpStatus.BAD_REQUEST);
         
-        Hys hys = new Hys(dtohys.getNombre(),dtohys.getPorcentaje());
-        sHys.save(hys);
+        Persona persona = new Persona(dtopersona.getNombre(),dtopersona.getApellido(),dtopersona.getImg(),dtopersona.getDescripcion());
+        personaService.save(persona);
         return new ResponseEntity(new Mensaje("Skill agregada"),HttpStatus.OK);
-    }*/
+    }
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id")int id,@RequestBody dtoPersona dtopersona){
         if(!personaService.existsById(id))
