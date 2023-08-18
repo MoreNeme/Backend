994d1a0b41d92edbf9c713592d089cfaeebb60a5
mariano <104591927+MoreNeme@users.noreply.github.com>
2023-08-18 19:01:58 -0300

    update

--- src/main/java/com/portfolio/mnc/controller/CEducacion.java
+++ src/main/java/com/portfolio/mnc/controller/CEducacion.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = {"https://frontend-46920.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
